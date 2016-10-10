713485066d037341aee52b076a7c6c2f8de07fbf
antonio <antonio@biruseto>
2016-10-10 09:37:12 +0200

    primer commit

---
+++ src/ejemploobjetos/EjemploObjetos.java
@@ -0,0 +1,27 @@
+
+package ejemploobjetos;
+
+public class EjemploObjetos {
+    
+    public static void main(String[] args) {
+        //instanciamos//
+        Semaforo unsemaforo = new Semaforo();
+        
+        //le damos valor a la variable//
+        unsemaforo.ponColor("verde");
+       
+        String valor = unsemaforo.dameColor();// a traves del medoto dameColor, me esta devolviendo un valor de tipo String//
+        System.out.println("color ="+ valor);//visualizo pantalla resultado =null al no haberle dicho que sea verde//
+        
+        
+        Semaforo otroSemaforo = new Semaforo();//creamos otro semaforo con color ambar//
+        otroSemaforo.ponColor("ambar");
+        //visualizo de nuevo//
+        System.out.println("color2 ="+otroSemaforo.dameColor());
+        
+        
+        
+        
+    }
+    
+}
